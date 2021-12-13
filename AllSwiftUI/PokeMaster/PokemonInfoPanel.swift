be99580524893abd8709f46b9e4014bcec26870c
Renjun Li <renjunli@Renjuns-MacBook-Pro.local>
2021-12-13 22:10:18 +0800

    update

---
+++ AllSwiftUI/PokeMaster/PokemonInfoPanel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonInfoPanel.swift
+//  AllSwiftUI
+//
+//  Created by Renjun Li on 2021/12/13.
+//
+
+import Foundation
---
+++ AllSwiftUI/PokeMaster/PokemonInfoRow.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonInfoRow.swift
+//  AllSwiftUI
+//
+//  Created by Renjun Li on 2021/12/12.
+//
+
+import Foundation
