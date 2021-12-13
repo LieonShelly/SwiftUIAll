2c9d15ff8e51ff91a8fc7481359f81abceedbf5a
Renjun Li <renjunli@Renjuns-MacBook-Pro.local>
2021-12-13 23:29:58 +0800

    update

---
+++ AllSwiftUI/Setting/SettingView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingView.swift
+//  AllSwiftUI
+//
+//  Created by Renjun Li on 2021/12/13.
+//
+
+import Foundation
---
+++ AllSwiftUI/Setting/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  AllSwiftUI
+//
+//  Created by Renjun Li on 2021/12/13.
+//
+
+import Foundation
